5d6a3fb1983bab579c44b37646537c7de0eccf55
lalaG <lalagaye@gmail.com>
2019-10-03 23:59:27 +0200

    creating the sign in page and the sign in form input and custom button components

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-import Header from './components/header/header.component'
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
@@ -13,9 +14,9 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
-    // <HomePage />
   );
 }
 
