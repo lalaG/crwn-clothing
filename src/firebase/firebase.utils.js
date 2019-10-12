4ef2307d7742e60906355a9f0e85572ef067651e
lalaG <lalagaye@gmail.com>
2019-10-13 00:07:55 +0200

    storing users in firebase and in the app

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,15 +3,40 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-    apiKey: "AIzaSyALqyO8c7JwKnZjaEApwtt2j6LsZFfNBso",
-    authDomain: "crwn-clothing-aa689.firebaseapp.com",
-    databaseURL: "https://crwn-clothing-aa689.firebaseio.com",
-    projectId: "crwn-clothing-aa689",
-    storageBucket: "",
-    messagingSenderId: "3601644950",
-    appId: "1:3601644950:web:33df25c966c42f7ef031b7",
-    measurementId: "G-WS0QSVL064"
+    apiKey: "AIzaSyDQOstLA6ex0f5xghrNaELs7XFGm4VJsEg",
+    authDomain: "crwn-clothing-19ff8.firebaseapp.com",
+    databaseURL: "https://crwn-clothing-19ff8.firebaseio.com",
+    projectId: "crwn-clothing-19ff8",
+    storageBucket: "crwn-clothing-19ff8.appspot.com",
+    messagingSenderId: "239616684745",
+    appId: "1:239616684745:web:6231dd234a9ce448f89318",
+    measurementId: "G-R0SJMC4CTX"
 };
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if(!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const snapShot = await userRef.get();
+    
+    if(!snapShot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            });
+        } catch (error) {
+            console.log('error creating user', error.message);
+        }
+    }
+
+    return userRef;
+}
 
 firebase.initializeApp(config);
 
